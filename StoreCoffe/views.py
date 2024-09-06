1abbe2af8a5fde7f90653471fe23058be0f193f7
sebastian-p-c <seb.porma@duocuc.cl>
2024-09-06 00:10:39 -0400

    nuevos productos

--- StoreCoffe/views.py
+++ StoreCoffe/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def index(request):
     context={}
     return render(request, 'menu/index.html', context)
+
+def nosotros(request):
+    context={}
+    return render(request, 'menu/nosotros.html', context)
